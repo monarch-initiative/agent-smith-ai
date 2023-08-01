749d4f62c75b53e8fbbec4e2263bc2384d1b0f1f
Shawn O'Neil <oneilsh@gmail.com>
2023-08-01 15:19:33 -0700

    added token count functionality to base model

--- src/monarch_assistant/utility_agent.py
+++ src/monarch_assistant/utility_agent.py
@@ -8,12 +8,11 @@ from typing import Any, Dict, List, Union, Literal, get_args, get_origin
 # Third party imports
 from docstring_parser import parse
 import openai
+import tiktoken
 
 # Local application imports
 from monarch_assistant.openapi_wrapper import APIWrapperSet 
 from monarch_assistant.models import *
-
-
 
 
 
@@ -34,7 +33,7 @@ class UtilityAgent:
         self.api_set = APIWrapperSet([])
         self.callable_methods = []
 
-        self.register_callable_methods(["time", "print_history", "help"])
+        self.register_callable_methods(["time", "print_history", "help", "count_history_tokens"])
 
     def register_api(self, name: str, spec_url: str, base_url: str, callable_endpoints: List[str] = []):
         self.api_set.add_api(name, spec_url, base_url, callable_endpoints)
@@ -62,6 +61,14 @@ class UtilityAgent:
         else:
             raise ValueError(f"No such method: {method_name}")
 
+
+    def count_history_tokens(self) -> int:
+        """
+        Uses the tiktoken library to count the number of tokens stored in self.history.
+        """
+        #print(self._reserialize_chat(self.history))
+        return _num_tokens_from_messages(self._reserialize_chat(self.history), model = self.model)
+        #return sum([len(encoding.encode(message.content)) for message in self.history.messages])
 
     def help(self):
         """Returns information about this agent, including a list of callable methods and functions."""
@@ -343,3 +350,46 @@ def _generate_schema(fn):
             'description': p.description
         }
     return schema
+
+
+
+## Straight from https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+def _num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
+    """Return the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model in {
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-16k-0613",
+        "gpt-4-0314",
+        "gpt-4-32k-0314",
+        "gpt-4-0613",
+        "gpt-4-32k-0613",
+        }:
+        tokens_per_message = 3
+        tokens_per_name = 1
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif "gpt-3.5-turbo" in model:
+        print("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
+        return _num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
+    elif "gpt-4" in model:
+        print("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
+        return _num_tokens_from_messages(messages, model="gpt-4-0613")
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
+        )
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(str(value)))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
