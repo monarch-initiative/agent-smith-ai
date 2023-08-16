50ee99c347f21d20e327a10fd5ba91742056d40a
Shawn O'Neil <oneilsh@gmail.com>
2023-08-15 20:01:38 -0700

    set version in package

--- src/agent_smith_ai/__init__.py
+++ src/agent_smith_ai/__init__.py
@@ -5,4 +5,4 @@ try:
     __version__ = importlib_metadata.version(__name__)
 except importlib_metadata.PackageNotFoundError:
     # package is not installed
-    __version__ = "0.0.0"  # pragma: no cover
+    __version__ = "0.9.0"  # pragma: no cover
