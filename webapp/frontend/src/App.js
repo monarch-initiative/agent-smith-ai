17d1b75cfaebe3b1592a0b798a1366cb69392e61
Shawn O'Neil <oneilsh@gmail.com>
2023-08-18 22:00:00 -0700

    ui improvements

--- webapp/frontend/src/App.js
+++ webapp/frontend/src/App.js
@@ -1,34 +1,84 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
 import './App.css';
 
 function App() {
   const [chatLog, setChatLog] = useState([]);
   const [question, setQuestion] = useState('');
+  const [showFunctionMessages, setShowFunctionMessages] = useState(true);
+  const ws = useRef(null);
 
-  const handleChat = async () => {
-    const response = await fetch('http://localhost:8000/chat/', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({question: "this is a test"})
-    });
-    const data = await response.json();
-    setChatLog([...chatLog, ...data.responses]);
-    setQuestion('');  // Clear the input after sending
+  const getWebSocketUrl = () => {
+    const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+    return `${protocol}//${window.location.host}/ws/chat/`;
+  }
+
+  const getSessionId = () => {
+    return localStorage.getItem("sessionId");
+  }
+
+  useEffect(() => {
+    ws.current = new WebSocket(getWebSocketUrl());
+
+    ws.current.onopen = () => console.log("ws opened");
+    ws.current.onmessage = (e) => {
+        const messageData = JSON.parse(e.data);
+        setChatLog((prevLog) => [...prevLog, messageData]);
+    };
+    ws.current.onclose = () => {
+        console.log("ws closed");
+        // Here you can implement logic to handle a reconnect if necessary.
+    };
+
+    return () => {
+        if (ws.current && ws.current.readyState === WebSocket.OPEN) {
+            ws.current.close();
+        }
+    };
+  }, []);
+
+  const handleChat = () => {
+    const message = {
+        question: question,
+        session_id: getSessionId()
+    };
+
+    if (ws.current.readyState !== WebSocket.OPEN) {
+        console.error("WebSocket is not open. Can't send message.");
+        // Optionally, you can implement logic here to handle a reconnect.
+        return;
+    }
+    
+    ws.current.send(JSON.stringify(message));
   };
+
 
   return (
     <div className="App">
       <div className="chatWindow">
-        {chatLog.map((message, index) => (
-          <div key={index} className={`message ${message.role}`}>
-            {message.content}
-          </div>
-        ))}
+        {chatLog.map((message, index) => {
+          if (!showFunctionMessages && message.role === "function") {
+            return null;
+          }
+
+          const content = message.content || (message.is_function_call ? `${message.func_name}(${JSON.stringify(message.func_arguments)})` : '');
+
+          return (
+            <div key={index} className={`message ${message.role}`}>
+              <ReactMarkdown remarkPlugins={[gfm]}>{content}</ReactMarkdown>
+            </div>
+          );
+        })}
       </div>
       <div className="chatInput">
         <input value={question} onChange={e => setQuestion(e.target.value)} placeholder="Type your message..."/>
         <button onClick={handleChat}>Send</button>
       </div>
+      <label>
+        <input type="checkbox" checked={showFunctionMessages} onChange={() => setShowFunctionMessages(!showFunctionMessages)} />
+        Show function messages
+      </label>
     </div>
   );
 }
--- webapp/main.py
+++ webapp/main.py
@@ -1,32 +1,67 @@
 import os
 from pathlib import Path
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends, WebSocket
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from pathlib import Path
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from agent_smith_ai.utility_agent import UtilityAgent
+from uuid import uuid4
 
 import dotenv
 dotenv.load_dotenv()
 
 app = FastAPI()
 
+# allow CORS requests from localhost:3000
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 ##### API #####
 
-class ChatRequest(BaseModel):
-    question: str
+# In-memory storage for agent states
+SESSIONS = {}
 
-@app.post("/chat/")
-async def chat(request_data: ChatRequest):
-    question = request_data.question
-    agent = UtilityAgent(name="Webapp Agent")
-    responses = agent.new_chat(question, yield_prompt_message=True)
-    messages = [message.model_dump() for message in responses]
-    return {"responses": messages}
+# Define a dependency to get or create the agent state for a session
+def get_agent_state(request: Request):
+    session_id = request.headers.get("X-Session-ID")
+    if not session_id:
+        session_id = str(uuid4())  # Generate a new session ID if not provided
+    agent = SESSIONS.get(session_id)
+    if not agent:
+        agent = UtilityAgent(name="Webapp Agent")
+        SESSIONS[session_id] = agent
+    return agent, session_id
 
+
+
+@app.websocket("/ws/chat/")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_json()
+
+        question = data.get("question")
+
+        session_id = data.get("session_id")
+        if not session_id:
+            session_id = str(uuid4())
+
+
+        agent = SESSIONS.get(session_id)
+        if not agent:
+            agent = UtilityAgent(name="Webapp Agent")
+            SESSIONS[session_id] = agent
+        
+        responses = agent.new_chat(question, yield_prompt_message=True)
+        for message in responses:
+            await websocket.send_json(message.model_dump())
 
 
 
